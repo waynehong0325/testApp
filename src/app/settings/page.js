4701c8225cc1e5d2ad6dda467717b6e1e7c624e4
2300015 <2300015@systex.com.tw>
2024-07-30 11:31:24 +0800

    chore: add

---
+++ src/app/settings/page.js
@@ -0,0 +1,9 @@
+
+function Home() {
+  return (
+    <main className="flex items-center justify-center p-24 text-lg">
+      <div className="m-3">settings</div>
+    </main>
+  )
+}
+export default Home
