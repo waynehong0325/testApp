737d97a3589d1aadc5dfec7b5835151ec3262b24
2300015 <2300015@systex.com.tw>
2024-07-30 11:25:19 +0800

    chore: add

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+"use client"
+import { useState } from "react"
 function Home() {
+  const [state,setAdd]=useState(0)
+  const handleAdd =()=>{
+    const newState = state+1
+    setAdd(newState)
+  }
   return (
-    <main className="flex items-center justify-between p-24 text-lg">
-      Hello World
+    <main className="flex items-center justify-center p-24 text-lg">
+      <div className="m-3">count : {state}</div>
+      <button className="border border-slate-900" onClick={handleAdd}>add</button>
     </main>
   )
 }
